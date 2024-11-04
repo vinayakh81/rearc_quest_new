4fd9f17aa6db1a59e42112d2a7dc1f96bc751f4d
Vinayak Harti <vinayakh81@gmail.com>
2024-11-05 00:52:35 +0530

    all updated code

---
+++ App/src/server.js
@@ -0,0 +1,26 @@
+// index.js
+const express = require('express');
+const app = express();
+
+// Get the SECRET_WORD from the environment variable
+const secretWord = process.env.SECRET_WORD || 'No secret provided';
+
+// Root route
+app.get('/', (req, res) => {
+  res.send(`<h1>Welcome!</h1><p>Secret Word: ${secretWord}</p>`);
+});
+
+// Secret word route for Docker check
+app.get('/secret_word', (req, res) => {
+    res.json({ secretWord: secretWord });
+  });
+
+// Docker check route
+app.get('/docker', (req, res) => {
+  res.json({ status: 'Container is running', secretWord: secretWord });
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log(`App running on port 3000.`);
+});
